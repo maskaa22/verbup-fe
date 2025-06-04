44a8468e7fbdbb452031ecb143214c91b41cfcd0
Mariia Skydan <kyrosika565@gmail.com>
2025-06-04 12:22:57 +0300

    one game and progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Game from "./pages/game/Game";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
