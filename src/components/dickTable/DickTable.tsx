fb21d6702ecec9f4e5510639325782969746b740
Sasha Savenko <sashasavenko3@gmail.com>
2025-06-05 18:56:13 +0300

    icon added

--- src/components/dickTable/DickTable.tsx
+++ src/components/dickTable/DickTable.tsx
@@ -1,20 +1,22 @@
 import VERBS from "../../data/irr-verbs.json";
+import css from "./DickTable.module.css"
 
 
 const DickTable = () => {
     const verbs = VERBS.easy;
 return <div>
-    <table>
-  <thead>
+    <table className={css.table}>
+  {/* <thead>
     <tr>
       <th>Base Form</th>
       <th>Past Simple</th>
       <th>Past Participle</th>
     </tr>
-  </thead>
+  </thead> */}
   <tbody>
     {verbs.map((verb, idx) => (
       <tr key={idx}>
+        <td><svg className={css.icon}><use href="./icons.svg#icon-voc"></use></svg></td>
         <td>{verb.base_form}</td>
         <td>{verb.past_simple}</td>
         <td>{verb.past_participle}</td>
@@ -25,3 +27,4 @@ return <div>
 </div>
 };
 export default DickTable;
+
