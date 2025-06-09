c6de20f26948e33f2df58e5e5cab375fe4be861f
Sasha Savenko <sashasavenko3@gmail.com>
2025-06-09 19:49:13 +0300

    logo styles fixed

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,7 +2,7 @@ import css from "./Logo.module.css"
 
 const Logo = () => {
 return <div className={css.wrap}>
-    <h4>VerbUp!</h4>
+    <h4>Verb<span>Up!</span></h4>
     <p>Вивчай легко з нами</p>
 </div>
 };
