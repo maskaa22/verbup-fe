c6bb89420fb2e89229b51991e7fddee70cd4afbc
Sasha Savenko <sashasavenko3@gmail.com>
2025-07-02 17:03:59 +0300

    settings styles

--- src/pages/setting/Setting.tsx
+++ src/pages/setting/Setting.tsx
@@ -4,38 +4,62 @@ import css from "./Setting.module.css";
 
 const Setting = () => {
   return (
-    <div>
+    <div className={css.wrap}>
       <h2>Налаштування</h2>
 
       <h3>Профіль</h3>
-      <ul>
+      <ul className={css.list}>
         <li>
           <Link to={"/change-username"}>Змінити Імя</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
         <li>
           <Link to={"/change-password"}>Змінити пароль</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
       </ul>
 
       <h3>Інше</h3>
-      <ul>
+      <ul className={css.list}>
         <li>
           <Link to={"/notification-params"}>Параметри сповіщень</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
         <li>
           <Link to={"/"}>Контакти</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
         <li>
           <Link to={"/"}>Поділитись з друзями</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
         <li>
           <Link to={"/theme-switcher"}>Тема</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
         <li>
           <Link to={"/setting/game"}>Обрати інший рівень</Link>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
         <li>
           <button className={css.button}>Вийти з акаунту</button>
+          <svg>
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
         </li>
       </ul>
 
