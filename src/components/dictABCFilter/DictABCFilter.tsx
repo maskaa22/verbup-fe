1ec199f4abe137d12b9e27b9be487d2d5b6239d7
Sasha Savenko <sashasavenko3@gmail.com>
2025-07-03 18:33:06 +0300

    slider bug fixed

--- src/components/dictABCFilter/DictABCFilter.tsx
+++ src/components/dictABCFilter/DictABCFilter.tsx
@@ -42,7 +42,7 @@ const DictABCFilter = () => {
   }, []);
   return (
     <div className={css.wrap} ref={dropdownRef}>
-      <Swiper loop={false} slidesPerView={5} spaceBetween={10} className={css.list}>
+      <Swiper loop={false} slidesPerView={9} className={css.list}>
         {alphabet.map((letter, idx) => (
           <SwiperSlide
             className={clsx(css.li, active === letter && css.actv)}
