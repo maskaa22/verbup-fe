99f5b87eec4b7e127b01474a4025570dab8d36e7
Sasha Savenko <sashasavenko3@gmail.com>
2025-07-08 15:47:46 +0300

    styles

--- src/pages/themeSwitcher/ThemeSwitcher.tsx
+++ src/pages/themeSwitcher/ThemeSwitcher.tsx
@@ -1,50 +1,56 @@
 import clsx from "clsx";
-import css from "./ThemeSwitcher.module.css"
+import css from "./ThemeSwitcher.module.css";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { selectDarkTheme } from "../../redux/notify/selectors";
 import { setTheme } from "../../redux/notify/slice";
 import { useEffect } from "react";
 const ThemeSwitcher = () => {
-    const dispatch = useDispatch();
-    const theme = useSelector(selectDarkTheme);
-useEffect(() => console.log(theme), [theme])
+  const dispatch = useDispatch();
+  const theme = useSelector(selectDarkTheme);
+  useEffect(() => console.log(theme), [theme]);
 
-    return <div className={css.wrap}>
-<h2 className={css.title}>Тема</h2>
-<div className={css.theme}>
-    <div className={css.option}>
-    <label htmlFor="light">Світла тема</label>
-     <input
-                           id="light"
-                           className={css.input}
-                           type="radio"
-                           name="themeSwitcher"
-                           value="light"
-                           checked={theme === "light"}
-                           onChange={(e) => dispatch(setTheme(e.target.value))}
-                        //    disabled={disabled}
-                         />
-                         <div className={css.switch}>
-                <span className={clsx(css.slider, css.round) }></span></div>
-                </div>
-                <div className={css.option}>
-    <label htmlFor="dark">Темна тема</label>
-     <input
-                           id="dark"
-                           className={css.input}
-                           type="radio"
-                           name="themeSwitcher"
-                           value="dark"
-                           checked={theme === "dark"}
-                          onChange={(e) => dispatch(setTheme(e.target.value))}
-                        //    disabled={disabled}
-                         />
-                         <div className={css.switch}>
-                <span className={clsx(css.slider, css.round) }></span></div>
-                </div>
-</div>
+  return (
+    <div className={css.wrap}>
+      <h2 className={css.title}>Тема</h2>
+      <div className={css.theme}>
+        <div className={css.option}>
+          <p>Світла тема</p>
+          
+          <label htmlFor="light" className={css.switch}>
+            <input
+            id="light"
+            className={css.input}
+            type="radio"
+            name="themeSwitcher"
+            value="light"
+            checked={theme === "light"}
+            onChange={(e) => dispatch(setTheme(e.target.value))}
+            //    disabled={disabled}
+          />
+            <span className={clsx(css.slider, css.round)}></span>
+          </label>
+        </div>
+        <div className={css.option}>
+          <p>Темна тема</p>
+          
+          <label htmlFor="dark" className={css.switch}>
+            <input
+            id="dark"
+            className={css.input}
+            type="radio"
+            name="themeSwitcher"
+            value="dark"
+            checked={theme === "dark"}
+            onChange={(e) => dispatch(setTheme(e.target.value))}
+            //    disabled={disabled}
+          />
+            <span className={clsx(css.slider, css.round)}></span>
+          </label>
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default ThemeSwitcher;
