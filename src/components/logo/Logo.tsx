327b2af8f6aae5cef4f62a74748875a1ad460415
Sasha Savenko <sashasavenko3@gmail.com>
2025-07-14 21:07:34 +0300

    verb list fixed

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,13 +1,8 @@
-import { useSelector } from "react-redux";
 import css from "./Logo.module.css"
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Logo = () => {
-    const isloggedIn = useSelector(selectIsLoggedIn)
 return <div className={css.wrap}>
     <img src="/image/Logo-full-min.png" alt="planet above words verb up!" />
-    {/* <h4>Verb<span>Up!</span></h4> */}
-    {isloggedIn ? <p>user is logged in</p> : <p>not logged in</p> }
 </div>
 };
 export default Logo;
