f0f330010d06f3676b346aee166cf636bd6cf198
Sasha Savenko <sashasavenko3@gmail.com>
2025-07-14 17:11:47 +0300

    goodbuy react-accordion library from dictItem

--- src/components/dictItem/DictItem.tsx
+++ src/components/dictItem/DictItem.tsx
@@ -1,69 +1,48 @@
+import { useState } from "react";
 import type { Props } from "../../utils/dict/dictTypes";
 import { speakText } from "../../utils/voiseFunction";
 import css from "./DictItem.module.css";
-import { Accordion, AccordionItem } from "@szhsin/react-accordion";
-
-
 
 const DictItem: React.FC<Props> = ({
   word: { base_form, past_simple, past_participle, uk },
 }) => {
-  const wordList = `${base_form} ${past_simple} ${past_participle}` 
-const handleSound = (word: string) => {
-  console.log("sound")
-  speakText(word, true)
-}
-  return (
-    <Accordion className={css.wordWrap}>
-      <AccordionItem style={{width: "100%"}}
-        header={({ state: { isEnter } }) => (
-          <div className={css.bsWrap}>
-            <div className={css.dotWrap}>
-            <span></span>
-            <div className={css.baseForm}>
-              <p>{base_form}</p>
-              <p>{uk}</p>
-            </div>
-            </div>
-            <div onClick={() => handleSound(wordList)} className={css.soundBtn}>
-            <svg className={css.sound}>
-              <use href="./icons.svg#icon-sound"></use>
-            </svg>
-            </div>
-            <svg
-              className={css.arrow}
-              style={{
-                transform: isEnter ? "rotate(180deg)" : "rotate(0deg)",
-                transition: "0.3s",
-              }}
-            >
-              <use href="./icons.svg#icon-accordion-arrow"></use>
-            </svg>
+  const [isOpen, setIsOpen] = useState(false);
+  const wordList = `${base_form} ${past_simple} ${past_participle}`;
+  const handleSound = (word: string) => speakText(word, true);
+  const handleToggle = () => setIsOpen(!isOpen)
+  return ( <div className={css.dictItem}>
+    <div onClick={(e) => e.stopPropagation()} className={css.bsWrap}>
+        <div className={css.dotWrap}>
+          <span></span>
+          <div className={css.baseForm}>
+            <p>{base_form}</p>
+            <p>{uk}</p>
           </div>
-        )}
-        buttonProps={{ className: css.customButton }}
-      >
-        <div className={css.irrForm}>
-          <p>{past_simple}</p>
-          <p>{past_participle}</p>
         </div>
-      </AccordionItem>
-    </Accordion>
+        <div onClick={() => handleSound(wordList)} className={css.soundBtn}>
+          <svg className={css.sound}>
+            <use href="./icons.svg#icon-sound"></use>
+          </svg>
+        </div>
+        <div onClick={() => handleToggle()}>
+          <svg
+            className={css.arrow}
+            style={{
+              transform: isOpen ? "rotate(180deg)" : "rotate(0deg)",
+              transition: "0.3s",
+            }}
+          >
+            <use href="./icons.svg#icon-accordion-arrow"></use>
+          </svg>
+        </div>
+      </div>
+      {isOpen && <div className={css.irrForm}>
+      <p>{past_simple}</p>
+      <p>{past_participle}</p>
+    </div>}
+      </div>
   );
 };
 
 export default DictItem;
 
-{
-  /* <div className={css.dictItem}>
-      <div onClick={handleShowForms} className={css.baseForm}>
-        <p>{base_form}</p>
-        <p>{uk}</p>
-      </div>
-      <div className={clsx(css.irrForm, hideForms && css.hidden)}>
-        <p>{past_simple}</p>
-        <p>{past_participle}</p>
-      </div>
-      <div onClick={handleShowForms}>+</div>
-    </div> */
-}
