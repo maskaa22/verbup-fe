ae567836c252f63d2c95acaa54e0df876bcf7dae
Mariia Skydan <kyrosika565@gmail.com>
2025-07-28 15:40:33 +0300

    fix wpa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', '/run.png', 'robots.txt'],
+      manifest: {
+        name: 'VerbUp',
+        short_name: 'VerbUp',
+        description: 'VerbUp',
+        theme_color: '#5E909E',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: 'https://verbup-fe.vercel.app/',
+        icons: [
+          {
+            src: '/run.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/run.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // дозволяє працювати у dev-режимі
+      },
+    }),
   ],
-})
+});
