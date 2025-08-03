829cadcf79378b27d5885bebad67ab5a2a861016
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-03 11:11:31 +0300

    InputPassword

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,11 +1,12 @@
-import { type FormikHelpers } from "formik";
-// import s from "./RegistraterForm.module.css";
+import { Formik, Form, type FormikHelpers } from "formik";
+import css from "./RegistraterForm.module.css";
 import { useDispatch } from "react-redux";
 import type { AppDispatch } from "../../redux/store";
 import { register } from "../../redux/auth/operations";
-import FormTemplate from "../formForm/FormTemplate";
-import { arrOfNamesSignup } from "../../constants";
 import type { RegFormValues } from "../../utils/formTypes";
+import BaseButtonStart from "../baseButtonStart/BaseButtonStart";
+import FormInput from "../formInput/FormInput";
+import FormInputPassword from "../formInputPassword/FormInputPassword";
 
 const RegisterForm: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -16,23 +17,25 @@ const RegisterForm: React.FC = () => {
     dispatch(register(values));
     actions.resetForm();
   };
-
-  const props = {
-    arrOfNames: arrOfNamesSignup,
-    onSubmit: handleSubmit,
-  };
-  return <FormTemplate {...props} />;
+  return  <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
+        <FormInput label="Ім'я" name="username" type="text" placeholder="Введіть ваше ім'я" icon="icon-user"/>
+        <FormInput label="E - mail" name="email" type="email" placeholder="your@email.com" icon="icon-email"/>
+        <FormInputPassword/>
+        <BaseButtonStart label="Зарееструватися"/>
+      </Form>
+    </Formik> ;
 };
 
 export default RegisterForm;
 
-{
-  /* <Formik
-      initialValues={{ username: "", email: "", password: "" }}
-      onSubmit={handleSubmit}
-    >
-      <Form className={s.form}>
-        <label htmlFor="name">Ім'я</label>
+
+
+
+{/* <label htmlFor="name">Ім'я</label>
         <div>
           <Field type="text" name="username" placeholder="Введіть ваше ім'я" />
           <svg className={s.icon}>
@@ -45,19 +48,23 @@ export default RegisterForm;
           <svg className={s.icon}>
             <use href="./icons.svg#icon-email"></use>
           </svg>
-        </div>
-        <label htmlFor="password">Пароль</label>
-        <div>
-          <Field
-            type="password"
-            name="password"
-            placeholder="Мінімум 8 символів"
-          />
-          <svg className={s.icon}>
-            <use href="./icons.svg#icon-password"></use>
-          </svg>
-        </div>
-        <BaseButtonStart label="Зарееструватися"/>
-      </Form>
-    </Formik> */
-}
+        </div> */}
+
+
+  // const props = {
+  //   arrOfNames: arrOfNamesSignup,
+  //   onSubmit: handleSubmit,
+  // };
+
+
+  // <label htmlFor="password">Пароль</label>
+  //       <div>
+  //         <Field className={css.input}
+  //           type="password"
+  //           name="password"
+  //           placeholder="Мінімум 8 символів"
+  //         />
+  //         <svg className={css.icon}>
+  //           <use href="./icons.svg#icon-password"></use>
+  //         </svg>
+  //       </div>
--- src/components/formInput/FormInput.tsx
+++ src/components/formInput/FormInput.tsx
@@ -2,15 +2,16 @@ import { Field } from "formik";
 import css from "./FormInput.module.css"
 
 interface Props {
+  label: string
   name: string
   type?: string
   placeholder?: string
   icon: string
 }
 
-const FormInput: React.FC<Props> = ({ name, type = 'text', placeholder = 'input', icon }) => {
+const FormInput: React.FC<Props> = ({ label, name, type = 'text', placeholder = 'input', icon }) => {
 return <div>  
-  <label htmlFor="name">E - mail</label>
+  <label htmlFor="name">{label}</label>
 <div className={css.inputWrap}>
 
           <Field className={css.input} id={name} type={type} name={name} placeholder={placeholder} />
