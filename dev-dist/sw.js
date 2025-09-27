29f9bc87812735a2dc8ec5e31d48194ac62efc39
Mariia Skydan <kyrosika565@gmail.com>
2025-09-27 11:41:21 +0300

    change sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.49irrloirfg"
+    "revision": "0.6dc1pp3qek8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
