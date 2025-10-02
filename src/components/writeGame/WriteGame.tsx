172e515c15023ce42d43d30cbf5cc3ad4af1b874
Mariia Skydan <kyrosika565@gmail.com>
2025-10-02 19:54:40 +0300

    fix error questions from write game

--- src/components/writeGame/WriteGame.tsx
+++ src/components/writeGame/WriteGame.tsx
@@ -5,12 +5,23 @@ import Keyboard from "../keyboard/Keyboard";
 import { handleBackspace, handleKeyPress } from "../../utils/gameFunctions";
 import BaseButtonGame from "../baseButtonGame/BaseButtonGame";
 import { useNavigate, useOutletContext } from "react-router-dom";
-import type { AnswerStatus, cardGameType, currentAnswerAndQuestions } from "../../utils/gameType";
+import type {
+  AnswerStatus,
+  cardGameType,
+  currentAnswerAndQuestions,
+} from "../../utils/gameType";
 import { useCountWord } from "../../hooks/gameHooks";
 import { useSelector } from "react-redux";
 import { selectCurrent } from "../../redux/game/selectors";
 import CardGame from "../cardGame/CardGame";
-import { ANSWER_STATUS, CORRECT, ERROR, LAST_INDEX, SUCCESS, WRONG } from "../../constants";
+import {
+  ANSWER_STATUS,
+  CORRECT,
+  ERROR,
+  LAST_INDEX,
+  SUCCESS,
+  WRONG,
+} from "../../constants";
 import { useDispatch } from "react-redux";
 import type { AppDispatch } from "../../redux/store";
 import { hydrateFromStorage } from "../../redux/game/slice";
@@ -25,31 +36,29 @@ const WriteGame = () => {
 
   const { questions } = useOutletContext<currentAnswerAndQuestions>();
 
-
   const current = useSelector(selectCurrent);
 
   const question = questions[current];
 
-  
   const count = useCountWord();
   const [answerStatuses, setAnswerStatuses] = useState<AnswerStatus[]>(() => {
-  const savedStatuses = sessionStorage.getItem(ANSWER_STATUS);
-  if (savedStatuses) return JSON.parse(savedStatuses);
-  return Array(count).fill("pending");
-});
+    const savedStatuses = sessionStorage.getItem(ANSWER_STATUS);
+    if (savedStatuses) return JSON.parse(savedStatuses);
+    return Array(count).fill("pending");
+  });
 
-useEffect(() => {
-  sessionStorage.setItem(ANSWER_STATUS, JSON.stringify(answerStatuses));
-}, [answerStatuses]);
-  
+  useEffect(() => {
+    sessionStorage.setItem(ANSWER_STATUS, JSON.stringify(answerStatuses));
+  }, [answerStatuses]);
+
   // console.log(isChecked, visibility);
 
-// const imgWrite = `/image/game/${question.basic}.png`;
+  // const imgWrite = `/image/game/${question.basic}.png`;
 
   const dispatch = useDispatch<AppDispatch>();
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
- useEffect(() => {
+  useEffect(() => {
     const correct = Number(sessionStorage.getItem(CORRECT)) || 0;
     const wrong = Number(sessionStorage.getItem(WRONG)) || 0;
     const current = Number(sessionStorage.getItem(LAST_INDEX)) || 0;
@@ -77,20 +86,14 @@ useEffect(() => {
       navigate("/game/result");
     }
   }, [current, questions, navigate]);
-console.log(answerStatuses);
 
   return (
     <>
       <div className={s.boxModel}>
-        {question && <CardGame question={question} answerStatusesWrite={answerStatuses}/>}
-        {/* <BaseComponentGame
-          current={current}
-          img={imgWrite}
-          question={question.question}
-          answerStatuses={answerStatuses}
-          count={count}
-           translate={question.translate}
-        /> */}
+        {question && (
+          <CardGame question={question} answerStatusesWrite={answerStatuses} />
+        )}
+
         <div className={s.inputContainer}>
           <input type="text" value={text} readOnly className={s.gameInput} />
         </div>
@@ -101,19 +104,21 @@ console.log(answerStatuses);
         onBackspace={() => handleBackspace(setText)}
       />
 
-      <BaseButtonGame
-        word={text}
-        setVisibility={setVisibility}
-        setShowCheckAnswer={setShowCheckAnswer}
-        setCheckAnswerType={setCheckAnswerType}
-        setModalActive={setModalActive}
-        correctAnswer={question.correctAnswer}
-        answerStatuses={answerStatuses}
-        setAnswerStatuses={setAnswerStatuses}
-        current={current}
-        setIsChecked={setIsChecked}
-        setText={setText}
-      />
+      {question && (
+        <BaseButtonGame
+          word={text}
+          setVisibility={setVisibility}
+          setShowCheckAnswer={setShowCheckAnswer}
+          setCheckAnswerType={setCheckAnswerType}
+          setModalActive={setModalActive}
+          correctAnswer={question.correctAnswer}
+          answerStatuses={answerStatuses}
+          setAnswerStatuses={setAnswerStatuses}
+          current={current}
+          setIsChecked={setIsChecked}
+          setText={setText}
+        />
+      )}
     </>
   );
 };
