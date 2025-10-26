ff30a8d5cb9753abc3453bffc8203569a1fb84b9
Sasha Savenko <sashasavenko3@gmail.com>
2025-10-26 16:26:04 +0200

    final position of stars

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a8vl9vvljvo"
+    "revision": "0.c0ttv59bnf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
