528b872c2a6d7512ca746b2d3efc1ec493fa9ec2
Mariia Skydan <kyrosika565@gmail.com>
2026-01-21 14:56:51 +0200

    add tooltop at settings

--- src/components/gameSettingRadio/GameSettingRadio.tsx
+++ src/components/gameSettingRadio/GameSettingRadio.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import css from "./GameSettingRadio.module.css";
 import type { RadioGroupProps } from "../../utils/gameType";
 import { verbFrom } from "../../constants";
+import Tooltip from "../tooltip/Tooltip";
 
 const GameSettingRadio: React.FC<RadioGroupProps> = ({
   name,
@@ -10,37 +11,48 @@ const GameSettingRadio: React.FC<RadioGroupProps> = ({
   selectedValue,
   needSpan,
   disabled,
-  disabledOption
+  disabledOption,
 }) => {
   return (
-    <div className={!disabled ? `${css.rset}` : `${css.rset} ${css.disabled}`}>
+    <div className={clsx(css.rset, disabled && css.disabled)}>
       {options.map((option, index) => {
-        const isOptionDisabled = disabled || (disabledOption ? disabledOption(option) : false);
-      return(
-        <div
-          className={clsx(
-            css.inputWrap,
-            selectedValue === option && css.inputWrapChecked,
-            isOptionDisabled && css.inputWrapDisabled
-          )}
-          key={index}
-        >
-          <label htmlFor={option} className={css.label} key={option}>
-            <span className={css.text}>{option}</span>
-            {needSpan && <span>{verbFrom[index]}</span>}
-          </label>
-          <input
-            id={option}
-            className={css.input}
-            type="radio"
-            name={name}
-            value={option}
-            checked={selectedValue === option}
-            onChange={() => onChange(option)}
-            disabled={disabled || (disabledOption ? disabledOption(option) : false)}
-          />
-        </div>
-      )})}
+        const isOptionDisabled =
+          disabled || (disabledOption ? disabledOption(option) : false);
+
+        const content = (
+          <div
+            className={clsx(
+              css.inputWrap,
+              selectedValue === option && css.inputWrapChecked,
+              isOptionDisabled && css.inputWrapDisabled
+            )}
+          >
+            <label htmlFor={option} className={css.label}>
+              <span className={css.text}>{option}</span>
+              {needSpan && <span>{verbFrom[index]}</span>}
+            </label>
+
+            <input
+              id={option}
+              className={css.input}
+              type="radio"
+              name={name}
+              value={option}
+              checked={selectedValue === option}
+              onChange={() => onChange(option)}
+              disabled={isOptionDisabled}
+            />
+          </div>
+        );
+
+        return isOptionDisabled ? (
+          <Tooltip key={option} text="Увійдіть">
+            {content}
+          </Tooltip>
+        ) : (
+          <div key={option}>{content}</div>
+        );
+      })}
     </div>
   );
 };
