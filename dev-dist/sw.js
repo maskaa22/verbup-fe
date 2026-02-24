437c4d726cc2c28cbcf2366ea27cfa2dc8975733
Mariia Skydan <kyrosika565@gmail.com>
2026-02-24 23:20:49 +0200

    add svg in dictionary

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8dbicg4qflg"
+    "revision": "0.1vgana9s988"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
